acedeb0fdc4448f87f202a7b6153abc858ef45ba
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2020-01-12 14:23:20 +0100

    Adds more replacement tokens

--- source/utils.ts
+++ source/utils.ts
@@ -1,6 +1,7 @@
 import * as fs from "fs";
 import * as fse from "fs-extra";
 import { Uri, window, workspace } from "vscode";
+import { pascalCase, paramCase, camelCase, snakeCase } from "change-case";
 
 import { Config, Template } from "./config";
 
@@ -8,35 +9,45 @@ export function getConfig() {
   return workspace.getConfiguration("module-templates") as Config;
 }
 
+const replaceTokens = (input: string, newValue: string) =>
+  input
+    .replace(/{name\.raw}/g, newValue)
+    .replace(/{name\.pascal}/g, pascalCase(newValue))
+    .replace(/{name\.kebab}/g, paramCase(newValue))
+    .replace(/{name\.camel}/g, camelCase(newValue))
+    .replace(/{name\.snake}/g, snakeCase(newValue));
+
 export function createFiles(
   moduleName: string,
   uri: Uri | undefined,
   template: Template
 ) {
-  // const root = workspace.workspaceFolders?.[0].uri.fsPath;
-  // const path = uri?.fsPath || template.defaultPath || root;
-  const root = uri?.fsPath || workspace.workspaceFolders?.[0].uri.fsPath;
-  const path =
-    template.defaultPath && root ? `${root}/${template.defaultPath}` : root;
+  const workspaceRoot = workspace.workspaceFolders?.[0].uri.fsPath;
 
-  if (!path) {
-    const msg =
-      "No output path found. Consider adding a 'defaultPath' to your template.";
-    window.showErrorMessage(msg);
-    throw new Error(msg);
+  if (!workspaceRoot) {
+    window.showErrorMessage(
+      "When running 'New From Template' from the command palette, you must be in a workspace"
+    );
+    return;
   }
 
+  const path = uri?.fsPath
+    ? uri.fsPath
+    : template.defaultPath
+    ? `${workspaceRoot}/${template.defaultPath}`
+    : workspaceRoot;
+
   const folderName = template.folder
-    ? template.folder.replace(/{name}/g, moduleName)
+    ? replaceTokens(template.folder, moduleName)
     : undefined;
   const fullPath = folderName ? `${path}/${folderName}` : path;
 
   fse.ensureDirSync(fullPath);
 
   template.files.forEach(({ name, content }) => {
-    const fileName = name.replace(/{name}/g, moduleName);
+    const fileName = replaceTokens(name, moduleName);
     const fileContent = content
-      .map(line => line.replace(/{name}/g, moduleName))
+      .map(line => replaceTokens(line, moduleName))
       .join("\n");
     fs.writeFileSync(`${fullPath}/${fileName}`, fileContent);
   });
