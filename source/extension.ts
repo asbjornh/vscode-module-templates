fd4fbc305dfe194b58d72c788181216773e09639
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2020-01-12 12:51:18 +0100

    Adds input box

--- source/extension.ts
+++ source/extension.ts
@@ -1,11 +1,21 @@
 "use strict";
 import { commands, ExtensionContext, window } from "vscode";
 
+async function newFromTemplate() {
+  const moduleName = await window.showInputBox({
+    prompt: "Enter module name"
+  });
+
+  if (!moduleName) return;
+
+  window.showInformationMessage(moduleName);
+}
+
 export function activate(context: ExtensionContext) {
   const commandId = "extension.newModuleFromTemplate";
 
   const command = commands.registerCommand(commandId, () => {
-    window.showInformationMessage("Hey");
+    newFromTemplate();
   });
 
   context.subscriptions.push(command);
